a902ab85c55711005a35dbbc2a5000f6ac07b0ac
Levi Klingler <klingler27@gmail.com>
2025-06-13 23:10:38 -0400

    feat: assorted nvim plugins

--- .config/nvim/lua/levikl/packer.lua
+++ .config/nvim/lua/levikl/packer.lua
@@ -5,13 +5,13 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+  use('wbthomason/packer.nvim')
 
-  use {
+  use({
 	  'nvim-telescope/telescope.nvim', tag = '0.1.8',
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+  })
 
   use({
 	  'rose-pine/neovim',
@@ -23,4 +23,8 @@ return require('packer').startup(function(use)
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('nvim-treesitter/playground')
+  use('theprimeagen/harpoon')
+  use('mbbill/undotree')
+  use('tpope/vim-surround')
+  use('tpope/vim-fugitive')
 end)
