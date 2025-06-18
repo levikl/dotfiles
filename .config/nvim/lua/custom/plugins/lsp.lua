5195b87bb757d73c5146ce84e489d2984dea4f3a
Levi Klingler <klingler27@gmail.com>
2025-06-18 18:37:35 -0400

    feat: enable inline linter messages

--- .config/nvim/lua/custom/plugins/lsp.lua
+++ .config/nvim/lua/custom/plugins/lsp.lua
@@ -14,6 +14,7 @@ return {
           },
         },
       },
+      { "https://git.sr.ht/~whynothugo/lsp_lines.nvim" },
     },
     config = function()
       require("lspconfig").lua_ls.setup {}
@@ -36,6 +37,18 @@ return {
           end
         end,
       })
+
+      require("lsp_lines").setup()
+      vim.diagnostic.config { virtual_text = true, virtual_lines = false }
+
+      vim.keymap.set("", "<leader>l", function()
+        local config = vim.diagnostic.config() or {}
+        if config.virtual_text then
+          vim.diagnostic.config { virtual_text = false, virtual_lines = true }
+        else
+          vim.diagnostic.config { virtual_text = true, virtual_lines = false }
+        end
+      end, { desc = "Toggle lsp_lines" })
     end,
   }
 }
