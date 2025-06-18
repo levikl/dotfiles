37132fbe60eac27d764ce3c27c50246225982bfc
Levi Klingler <klingler27@gmail.com>
2025-06-18 00:18:22 -0400

    feat: map quickfix nav to <opt+j> and <opt+k>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,12 +5,19 @@ vim.opt.clipboard = "unnamedplus" -- pressing "p" pastes clipboard buffer
 vim.opt.number = true
 vim.opt.relativenumber = true
 
--- source the most recently saved version of current file
-vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
--- source the current line
-vim.keymap.set("n", "<space>x", ":.lua<CR>")
--- (in visual mode) source the current line
-vim.keymap.set("v", "<space>x", ":lua<CR>")
+-- these make option+<key> keymaps functional on mac
+vim.cmd.nmap('˙ <M-h>')
+vim.cmd.nmap('∆ <M-j>')
+vim.cmd.nmap('˚ <M-k>')
+vim.cmd.nmap('¬ <M-l>')
+vim.cmd.nmap('Ø <M-O>')
+vim.cmd.nmap('ø <M-o>')
+
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>") -- source the most recently saved version of current file
+vim.keymap.set("n", "<space>x", ":.lua<CR>")                -- source the current line (in normal mode)
+vim.keymap.set("v", "<space>x", ":lua<CR>")                 -- source the current line (in visual mode)
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
