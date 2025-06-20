6fb778e1143cd2c6f997ecf7c93513faa99c3088
Levi Klingler <levikl@geeked.gg>
2025-06-20 01:14:26 -0400

    feat: expand number of LSPs

---
+++ .config/nvim/plugin/keymaps.lua
@@ -0,0 +1,5 @@
+local set = vim.keymap.set
+
+set("n", "<space>tf", "<cmd>PlenaryBustedFile %<CR>")
+
+--vim.api.nvim_set_keymap("n", "<leader>t", "<Plug>PlenaryTestFile", { noremap = false, silent = false })
