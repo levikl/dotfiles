a582b76836018ba1d4ee19e2db1fb14ab76dca48
Levi Klingler <levikl@geeked.gg>
2025-06-23 00:34:19 -0400

    refactoring

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,5 @@
 require "custom.lazy"
 
-vim.opt.shiftwidth = 4
-vim.opt.clipboard = "unnamedplus" -- pressing "p" pastes clipboard buffer
-vim.opt.number = true
-vim.opt.relativenumber = true
-
 -- Highlight when yanking (copying) text
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
