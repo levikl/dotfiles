a96b40a9419c0583e6c35890aab9f3fcfbe63924
Levi Klingler <levikl@geeked.gg>
2025-07-21 04:16:17 -0400

    moving .config/nvim to its own repo

--- .config/nvim/plugin/options.lua
+++
@@ -1,7 +0,0 @@
-local set = vim.opt
-
-set.shiftwidth = 4
-set.clipboard = "unnamedplus" -- pressing "p" pastes clipboard buffer
-set.number = true
-set.relativenumber = true
-set.signcolumn = "yes:1" -- retains editor's leftmost column for error indicators, even when empty
