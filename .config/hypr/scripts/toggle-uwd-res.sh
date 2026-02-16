93c4007a055896ce884f282210e1623f460e7484
Levi Klingler <levikl@geeked.gg>
2026-02-16 13:53:47 -0500

    some raps

--- .config/hypr/scripts/toggle-uwd-res.sh
+++ .config/hypr/scripts/toggle-uwd-res.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # vim: filetype=bash
 
-hi_res=('1080x0' '0x-1440' '3440x-1400' '-1080x-1440' '0x0000FF84, preferred')
-md_res=('960x0' '0x-1080' '3440x-1080' '-1080x-1080' '0x0000FF84, 2560x1080@60')
-lo_res=('480x0' '0x-960' '3440x-960' '-1080x-960' '0x0000FF84, 2240x960@60')
+hi_res=('1080x0' ' 0x-1440' ' 3440x-1440,' '-1080x-1440' '0x0000FF84, preferred')
+md_res=('960x0' ' 0x-1080' ' 2560x-1080,' '-1080x-1080' '0x0000FF84, 2560x1080@60')
+# lo_res=('480x0' '0x-960' '3440x-960' '-1080x-960' '0x0000FF84, 2240x960@60')
 
 hyprland_conf=/home/levi/.config/hypr/hyprland.conf
 
@@ -11,13 +11,14 @@ if grep -q '\-1080x-1440' $hyprland_conf; then
   declare -n a=hi_res
   declare -n b=md_res
 else
-  if grep -q '\-1080x-1080' $hyprland_conf; then
-    declare -n a=md_res
-    declare -n b=lo_res
-  else
-    declare -n a=lo_res
-    declare -n b=hi_res
-  fi
+  # if grep -q '\-1080x-1080' $hyprland_conf; then
+  declare -n a=md_res
+  # declare -n b=lo_res
+  declare -n b=hi_res
+  # else
+  #   declare -n a=lo_res
+  #   declare -n b=hi_res
+  # fi
 fi
 
 for i in "${!a[@]}"; do
